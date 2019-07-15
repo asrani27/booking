d831a463d1222daac7c7bb340fa63d19e508cb4f
Asrani <asrani.27@gmail.com>
2019-07-15 13:59:32 +0800

    fix

--- resources/views/daftaranggota.blade.php
+++ resources/views/daftaranggota.blade.php
@@ -65,8 +65,10 @@
                             <div class="col-lg-6 contact_name_col">
                                 <input type="text" class="contact_input" name="alamat" placeholder="Alamat" placeholder="Alamat" required>
                             </div>
+                        </div>
+                        <div class="row">
                             <div class="col-lg-6">
-                                <input type="email" class="contact_input" name="email"  placeholder="Email Valid required>
+                                <input type="email" class="contact_input" name="email"  placeholder="Email Valid" required>
                             </div>
                         </div>
                         <div class="row">
