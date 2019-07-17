db629439ca6276416a590c4a3960b6675ae401d2
asrani <asrani.27@gmail.com>
2019-07-17 09:37:36 +0800

    fix

--- resources/views/bluesky/menu.blade.php
+++ resources/views/bluesky/menu.blade.php
@@ -9,11 +9,9 @@
 						</div>
 						<nav class="main_nav">
 							<ul>
-								{{-- <li class="active"><a href="index.html">< Portal</a></li> --}}
 								<li><a href="{{url('/')}}">Home</a></li>
 								<li><a href="{{url('agenda/komunitas')}}">Agenda Komunitas</a></li>
 								<li><a href="{{url('agenda/pemko')}}">Agenda Plaza</a></li>
-								{{-- <li><a href="{{url('booking')}}">Pesan</a></li> --}}
 								<li><a href="{{url('daftar')}}">Daftar</a></li>
 								<li><a href="{{url('login')}}">Login</a></li>
                             </ul>
@@ -36,19 +34,18 @@
 			<div class="logo menu_logo">
 				<a href="#">
 					<div class="logo_container d-flex flex-row align-items-start justify-content-start">
-						<div class="logo_image"><div><img src="{{url('bluesky/images/logo.png')}}" alt=""></div></div>
+						<div class="logo_image"><div><img src="{{url('bluesky/logopng.png')}}" alt=""></div></div>
 					</div>
 				</a>
 			</div>
 			<ul>
-				<li class="menu_item"><a href="index.html">Home</a></li>
-				<li class="menu_item"><a href="about.html">About us</a></li>
-				<li class="menu_item"><a href="#">Speakers</a></li>
-				<li class="menu_item"><a href="#">Tickets</a></li>
-				<li class="menu_item"><a href="news.html">News</a></li>
-				<li class="menu_item"><a href="contact.html">Contact</a></li>
+				<li class="menu_item"><a href="{{url('/')}}">Home</a></li>
+				<li class="menu_item"><a href="{{url('agenda/komunitas')}}">Agenda Komunitas</a></li>
+				<li class="menu_item"><a href="{{url('agenda/pemko')}}">Agenda Plaza</a></li>
+				<li class="menu_item"><a href="{{url('daftar')}}">Daftar</a></li>
+				<li class="menu_item"><a href="{{url('login')}}">Login</a></li>
 			</ul>
 		</div>
-		<div class="menu_phone"><span>call us: </span>652 345 3222 11</div>
+		{{-- <div class="menu_phone"><span>call us: </span>652 345 3222 11</div> --}}
 	</div>
 	
