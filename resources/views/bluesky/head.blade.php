521445cc83f8c98bf44dc6dec59728016fd8fc0b
asranim <asrani.27@gmail.com>
2020-03-04 11:20:35 +0800

    fix template

--- resources/views/bluesky/head.blade.php
+++ resources/views/bluesky/head.blade.php
@@ -2,7 +2,7 @@
     <title>Plaza SmartCity</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="Bluesky template project">
+    <meta name="description" content="Plaza Smart City">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="{{url('bluesky/styles/bootstrap4/bootstrap.min.css')}}">
     <link href="{{url('bluesky/plugins/font-awesome-4.7.0/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
